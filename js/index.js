62789f7e4405d5bb5288ddd9567fcd3411659d90
AlbaSalmeron <a.salmeronfarran@gmail.com>
2023-04-20 13:04:09 +0200

    web

--- js/index.js
+++ js/index.js
@@ -1,43 +1,134 @@
-const carrouselBtn       = document.querySelectorAll(`.Carrousel-btn`)
-const carrouselImg       = document.querySelectorAll(`.Carrousel-img`)
-const carrouselGaleria   = document.querySelector(`.Carrousel-galeria`)
-const next               = document.querySelector(`.Next`)
-const prev               = document.querySelector(`.Prev`)
+//Menu Mobile
 
+/* Este código agrega interactividad al menú de navegación móvil en la página de inicio de un sitio
+web. Selecciona elementos específicos con clases como `Home-svg`, `Home-navMobile`, `Home-imgMobile`
+y `Home-liMobile` y los asigna a constantes como `homeSvg`, `homeMobile`, `homeImg` y `hogarLi`.
+Luego agrega detectores de eventos a los elementos `homeSvg` y `homeImg` para el evento `click`, y a
+cada elemento `homeLi` para el evento `click`. Cuando se hace clic en el elemento `homeSvg`, se
+agrega la clase `isVisible` al elemento `homeMobile`, lo que hace visible el menú de navegación
+móvil. Cuando se hace clic en el elemento `homeImg`, se elimina la clase `isVisible` del elemento
+`homeMobile`, que oculta el menú de navegación móvil. Cuando se hace clic en cualquier elemento
+`homeLi`, también se elimina la clase `isVisible` del elemento `homeMobile`, que oculta el menú de
+navegación móvil. */
 
+const homeSvg=document.querySelector(`.Home-svg`)
+const homeMobile=document.querySelector(`.Home-navMobile`)
+const homeImg=document.querySelector(`.Home-imgMobile`)
+const homeLi=document.querySelectorAll(`.Home-liMobile`)
 
-// next.addEventListener(`click`,()=>{
-//     carrouselGaleria.style.transform = `translateX(-50%)`
-// })
-// prev.addEventListener(`click`,()=>{
-//     carrouselGaleria.style.transform = `translateX(-0%)`
-// })
-
-const numImg = carrouselImg.length
-const numImgVisibles = 3
-
-let carrouselContador = 0
-
-carrouselGaleria.style.width = `${ 100 * ( numImg /  numImgVisibles) }%`
-carrouselGaleria.style.gridTemplateColumns = `repeat( ${numImg} , 1fr )`
-
-const operacion = 100 / ( numImg / numImgVisibles )
-next.addEventListener(`click`,()=>{
-    
-    carrouselContador++
-    
-
-    if( carrouselContador >= ( numImg / numImgVisibles )){
-        carrouselContador = 0
-    }
-    carrouselGaleria.style.transform = `translateX(-${ operacion * carrouselContador  }%)`
+homeSvg.addEventListener(`click`,()=>{
+    homeMobile.classList.add(`isVisible`)
 })
-prev.addEventListener(`click`,()=>{
-    if( carrouselContador <= 0 ){
-        carrouselContador = ( numImg / numImgVisibles)
+homeImg.addEventListener(`click`,()=>{
+    homeMobile.classList.remove(`isVisible`)
+
+})
+
+homeLi.forEach((eachLi,i)=>{
+    homeLi[i].addEventListener(`click`,()=>{
+        homeMobile.classList.remove(`isVisible`)
+    })
+
+})
+
+//Works
+
+/* Este código agrega una función interactiva a la sección "Works" de una página web. Selecciona todos
+los elementos con la clase "Works-li" y "Works-img" y los asigna a las constantes "worksLi" y
+"worksImg", respectivamente. Luego agrega detectores de eventos a cada elemento "Works-li" para los
+eventos "mouseover" y "mouseout". Cuando el mouse está sobre un elemento "Works-li", elimina la
+clase "isActive" de todos los elementos "Works-img" y la agrega al elemento "Works-img"
+correspondiente. Cuando el mouse deja un elemento "Works-li", elimina la clase "isActive" de todos
+los elementos "Works-img". Esto crea un efecto de desplazamiento en el que la imagen correspondiente
+se resalta cuando el usuario pasa el cursor sobre un elemento "Works-li". */
+
+const worksLi=document.querySelectorAll(`.Works-li`)
+const worksImg=document.querySelectorAll(`.Works-img`)
+
+worksLi.forEach((eachLi,i)=>{
+    worksLi[i].addEventListener(`mouseover`,()=>{
+        worksImg.forEach((eachImg,j)=>{
+            worksImg[j].classList.remove(`isActive`)
+        })
+        worksImg[i].classList.add(`isActive`)
+    })
+    
+})
+worksLi.forEach((eachLi,i)=>{
+    worksLi[i].addEventListener(`mouseout`,()=>{
+       worksImg.forEach((eachImg,j)=>{
+        worksImg[j].classList.remove(`isActive`)
+       })
+
+    })
+
+})
+
+//Parallax
+
+/* Este código agrega un efecto de parallax a ciertos elementos de la página a medida que el usuario se
+desplaza. Selecciona elementos específicos y luego agrega un detector
+de eventos al objeto `window` para el evento `scroll`. Dentro del detector de eventos, obtiene la
+posición actual `scrollY` y `innerHeight` de la ventana. También obtiene la posición `offsetTop` del
+elemento `works`. */
+
+const nombre=document.querySelector(`.Nombre`)
+const apellido=document.querySelector(`.Apellido`)
+const homeP=document.querySelector(`.Home-p`)
+const about=document.querySelector(`.About`)
+const aboutH2=document.querySelector(`.About-h2`)
+const works=document.querySelector(`.Works`)
+
+window.addEventListener(`scroll`,()=>{
+    const { scrollY, innerHeight} = window
+    const {offsetTop} = works
+
+    nombre.style.transform=`translateY(${scrollY}px)`
+    apellido.style.transform=`translateY(${scrollY * 1.5}px)`
+    homeP.style.transform=`translateY(${scrollY * 6}px)`
+
+
+    if(scrollY >= offsetTop - (innerHeight / 2)){
+        worksLi.forEach((eachLi, i)=>{
+            worksLi[i].classList.add(`isActive`)
+        })  
+    }else{
+        worksLi.forEach((eachLi, i)=>{
+            worksLi[i].classList.remove(`isActive`)
+        })  
+
     }
 
-    carrouselContador--
+    const {top} = about.getBoundingClientRect()
+    if(top >= 0 ) {
+        aboutH2.classList.remove(`isActive`)
+    }else{
+        aboutH2.classList.add(`isActive`)
+    }
 
-    carrouselGaleria.style.transform = `translateX(-${ operacion * carrouselContador  }%)`
 })
+
+//Contact 
+    //background-color
+
+/* Este código selecciona todos los elementos con la clase `Contact-li` y los asigna a la constante
+`contactLi`. También está seleccionando el elemento con la clase `Contacto` y asignándolo a la
+constante `contacto`. */
+
+const contactLi=document.querySelectorAll(`.Contact-li`)
+const contact=document.querySelector(`.Contact`)
+
+contactLi.forEach((eachContact,i)=>{
+    contactLi[i].addEventListener(`mouseover`,()=>{
+        contact.classList.add(`isActive`)
+    })
+
+    contactLi[i].addEventListener(`mouseout`,()=>{
+        contact.classList.remove(`isActive`)
+
+    })
+})
+
+
+
+
---
+++ js/works.js
@@ -0,0 +1,63 @@
+//Project Carrousel
+
+/* Este código está creando una funcionalidad de carrusel para un conjunto de imágenes. Selecciona los
+elementos necesarios del HTML utilizando `document.querySelectorAll` y `document.querySelector`,
+calcula los valores necesarios para que funcione el carrusel y agrega detectores de eventos a los
+botones siguiente y anterior para mover el carrusel en consecuencia. La variable `carrouselContador`
+realiza un seguimiento de la posición actual del carrusel, y la variable `operacion` calcula el
+porcentaje de movimiento necesario para cada clic. */
+const carrouselBtn       = document.querySelectorAll(`.Carrousel-btn`)
+const carrouselImg       = document.querySelectorAll(`.Carrousel-img`)
+const carrouselGaleria   = document.querySelector(`.Carrousel-galeria`)
+const next               = document.querySelector(`.Next`)
+const prev               = document.querySelector(`.Prev`)
+
+const numImg = carrouselImg.length
+const numImgVisibles = 3
+
+let carrouselContador = 0
+
+carrouselGaleria.style.width = `${ 100 * ( numImg /  numImgVisibles) }%`
+carrouselGaleria.style.gridTemplateColumns = `repeat( ${numImg} , 1fr )`
+
+const operacion = 100 / ( numImg / numImgVisibles )
+next.addEventListener(`click`,()=>{
+    
+    carrouselContador++
+    
+
+    if( carrouselContador >= ( numImg / numImgVisibles )){
+        carrouselContador = 0
+    }
+    carrouselGaleria.style.transform = `translateX(-${ operacion * carrouselContador  }%)`
+})
+prev.addEventListener(`click`,()=>{
+    if( carrouselContador <= 0 ){
+        carrouselContador = ( numImg / numImgVisibles)
+    }
+
+    carrouselContador--
+
+    carrouselGaleria.style.transform = `translateX(-${ operacion * carrouselContador  }%)`
+})
+
+//Background color
+
+/* Este código agrega un detector de eventos de desplazamiento al objeto de la ventana. Cuando el
+usuario se desplaza, selecciona los elementos HTML con la clase "Proyecto" y "Proyecto-estrella"
+utilizando `document.querySelector` y `document.querySelectorAll`, respectivamente. Luego, agrega la
+clase "isActive" a estos elementos usando el método `classList.add()`. Es probable que esto se use
+para agregar algunos efectos visuales o animaciones a estos elementos cuando el usuario se desplaza. */
+
+const project=document.querySelector(`.Project`)
+const projectStar=document.querySelectorAll(`.Project-star`)
+window.addEventListener(`scroll`,()=>{
+    const {scrollY, innerHeight}=window
+
+    project.classList.add(`isActive`)
+    projectStar.forEach((eachStar,i)=>{
+        projectStar[i].classList.add(`isActive`)
+
+    })
+ 
+})
